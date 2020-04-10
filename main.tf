20e2e3ba64b584fa17dcbaba72448f1d72600dca
Eun Song <songew81@gmail.com>
2020-04-10 21:56:10 +0900

    Upgraded to terraform version 0.12 & added cors endpoint
    1. Did the overall updates to make it work with terraform version 0.12.
       For 0.11, created a feature branch called 0.11
    2. Added more terraform resources to create a mock endpoint for cors
       pre-flight request.

--- main.tf
+++ main.tf
@@ -2,40 +2,42 @@
 ## API Gateway Set Up
 ###########################################################
 resource "aws_api_gateway_rest_api" "apigw" {
-  name        = "${var.apigw_name}"
-  description = "${var.description}"
+  name                     = var.apigw_name
+  description              = var.description
+  minimum_compression_size = var.minimum_compression_size
 }
 
 resource "aws_api_gateway_resource" "main" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw.id}"
-  parent_id   = "${aws_api_gateway_rest_api.apigw.root_resource_id}"
-  path_part   = "${var.path_part}"
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  parent_id   = aws_api_gateway_rest_api.apigw.root_resource_id
+  path_part   = var.path_part
 }
 
 resource "aws_api_gateway_method" "main" {
-  rest_api_id   = "${aws_api_gateway_rest_api.apigw.id}"
-  resource_id   = "${aws_api_gateway_resource.main.id}"
-  http_method   = "${var.http_method}"
-  authorization = "${var.authorization}"
+  rest_api_id   = aws_api_gateway_rest_api.apigw.id
+  resource_id   = aws_api_gateway_resource.main.id
+  http_method   = var.http_method
+  authorization = var.authorization
 }
 
 resource "aws_api_gateway_integration" "main" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw.id}"
-  resource_id = "${aws_api_gateway_method.main.resource_id}"
-  http_method = "${aws_api_gateway_method.main.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  resource_id = aws_api_gateway_method.main.resource_id
+  http_method = aws_api_gateway_method.main.http_method
 
-  integration_http_method = "${var.integration_http_method}"
-  type                    = "${var.integration_type}"
-  uri                     = "${var.lambda_invoke_arn}"
+  # The integration HTTP method specifying how API Gateway 
+  # will interact with the back end. Not all methods are 
+  # compatible with all AWS integrations. e.g. 
+  # Lambda function can only be invoked via POST.
+  integration_http_method = var.integration_http_method
+  type                    = var.integration_type
+  uri                     = var.lambda_invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "apigw" {
-  depends_on = [
-    "aws_api_gateway_integration.main"
-  ]
-
-  rest_api_id = "${aws_api_gateway_rest_api.apigw.id}"
-  stage_name  = "${var.stage}"
+  depends_on  = [aws_api_gateway_integration.main]
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  stage_name  = var.stage
 }
 
 ###########################################################
@@ -44,7 +46,7 @@ resource "aws_api_gateway_deployment" "apigw" {
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = "${var.lambda_arn}"
+  function_name = var.lambda_arn
   principal     = "apigateway.amazonaws.com"
 
   # The /*/* portion grants access from any method on any resource
@@ -56,7 +58,7 @@ resource "aws_lambda_permission" "apigw" {
 ## Enabling API Gateway logs via CloudWatchLogs
 ###########################################################
 resource "aws_api_gateway_account" "apigw" {
-  cloudwatch_role_arn = "${aws_iam_role.apigw.arn}"
+  cloudwatch_role_arn = aws_iam_role.apigw.arn
 }
 
 resource "aws_iam_role" "apigw" {
@@ -76,11 +78,12 @@ resource "aws_iam_role" "apigw" {
   ]
 }
 EOF
+
 }
 
 resource "aws_iam_role_policy" "cloudwatch" {
   name = "${var.apigw_name}-apigw-cloudwatch-policy"
-  role = "${aws_iam_role.apigw.id}"
+  role = aws_iam_role.apigw.id
 
   policy = <<EOF
 {
@@ -102,11 +105,13 @@ resource "aws_iam_role_policy" "cloudwatch" {
     ]
 }
 EOF
+
 }
 
 resource "aws_api_gateway_method_settings" "apigw" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw.id}"
-  stage_name  = "${var.stage}"
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  stage_name  = var.stage
+
   #method_path = "${aws_api_gateway_resource.main.path_part}/${aws_api_gateway_method.main.http_method}"
   ## Currently there is a open bug with this resource which is that the mehtod settings are 
   ## not updated properly. An workaround is to set the method_path to "*/*".
@@ -114,42 +119,113 @@ resource "aws_api_gateway_method_settings" "apigw" {
   method_path = "*/*"
 
   settings {
-    metrics_enabled    = "${var.metrics_enabled}"
-    logging_level      = "${var.logging_level}"
-    data_trace_enabled = "${var.data_trace_enabled}"
+    metrics_enabled    = var.metrics_enabled
+    logging_level      = var.logging_level
+    data_trace_enabled = var.data_trace_enabled
   }
 
-  depends_on = ["aws_api_gateway_deployment.apigw"]
+  depends_on = [aws_api_gateway_deployment.apigw]
 }
 
 ###########################################################
 ## Custom Domain Set up
 ###########################################################
 resource "aws_api_gateway_domain_name" "apigw" {
-  count                    = "${var.custom_domain_enabled? 1 : 0}"
-  domain_name              = "${var.domain_name}"
-  regional_certificate_arn = "${var.regional_aws_acm_certificate_arn}"
+  count                    = var.custom_domain_enabled ? 1 : 0
+  domain_name              = var.domain_name
+  regional_certificate_arn = var.regional_aws_acm_certificate_arn
 
   endpoint_configuration {
-    types = [ "REGIONAL" ]
+    types = ["REGIONAL"]
   }
 }
 
 resource "aws_route53_record" "apigw" {
-  count   = "${var.custom_domain_enabled? 1 : 0}"
-  zone_id = "${var.aws_route53_zone_id}"
-  name    = "${aws_api_gateway_domain_name.apigw.domain_name}"
+  count   = var.custom_domain_enabled ? 1 : 0
+  zone_id = var.aws_route53_zone_id
+  name    = aws_api_gateway_domain_name.apigw[0].domain_name
   type    = "A"
 
   alias {
-    name                   = "${aws_api_gateway_domain_name.apigw.regional_domain_name}"
-    zone_id                = "${aws_api_gateway_domain_name.apigw.regional_zone_id}"
+    name                   = aws_api_gateway_domain_name.apigw[0].regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.apigw[0].regional_zone_id
     evaluate_target_health = true
   }
 }
 
 resource "aws_api_gateway_base_path_mapping" "apigw" {
-  count       = "${var.custom_domain_enabled? 1 : 0}"
-  api_id      = "${aws_api_gateway_rest_api.apigw.id}"
-  domain_name = "${aws_api_gateway_domain_name.apigw.domain_name}"
+  count       = var.custom_domain_enabled ? 1 : 0
+  api_id      = aws_api_gateway_rest_api.apigw.id
+  domain_name = aws_api_gateway_domain_name.apigw[0].domain_name
+  stage_name  = aws_api_gateway_deployment.apigw.stage_name
+}
+
+
+###########################################################
+## CORS endpoint
+###########################################################
+resource "aws_api_gateway_method" "cors" {
+  count = var.enable_cors ? 1 : 0
+
+  rest_api_id   = aws_api_gateway_rest_api.apigw.id
+  resource_id   = aws_api_gateway_resource.main.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "cors" {
+  count = var.enable_cors ? 1 : 0
+
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  resource_id = aws_api_gateway_method.main.resource_id
+  http_method = aws_api_gateway_method.cors[0].http_method
+
+  type = "MOCK"
+
+  request_templates = {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "cors" {
+  count = var.enable_cors ? 1 : 0
+
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  resource_id = aws_api_gateway_method.main.resource_id
+  http_method = aws_api_gateway_method.cors[0].http_method
+
+  status_code         = 200
+  response_parameters = {
+    "method.response.header.Content-Type"                 = "'application/json'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'${var.cors_origin}'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST, GET, OPTIONS, PUT, DELETE'",
+    "method.response.header.Access-Control-Allow-Headers" = "'Accept, Content-Type, Content-Length, Accept-Encoding, Authorization'",
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.cors,
+    aws_api_gateway_method_response.cors,
+  ]
+}
+
+resource "aws_api_gateway_method_response" "cors" {
+  count = var.enable_cors ? 1 : 0
+
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  resource_id = aws_api_gateway_method.main.resource_id
+  http_method = aws_api_gateway_method.cors[0].http_method
+
+  status_code         = 200
+  response_parameters = {
+    "method.response.header.Content-Type"                 = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Headers" = true,
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  depends_on = [aws_api_gateway_method.cors]
 }
--- output.tf
+++ output.tf
@@ -1,35 +1,36 @@
 output "invoke_url" {
-  value = "${aws_api_gateway_deployment.apigw.invoke_url}"
+  value = aws_api_gateway_deployment.apigw.invoke_url
 }
 
 output "execution_arn" {
-  value = "${aws_api_gateway_deployment.apigw.execution_arn}"
+  value = aws_api_gateway_deployment.apigw.execution_arn
 }
 
 output "deployment_created_date" {
-  value = "${aws_api_gateway_deployment.apigw.created_date}"
+  value = aws_api_gateway_deployment.apigw.created_date
 }
 
 output "deployment_id" {
-  value = "${aws_api_gateway_deployment.apigw.id}"
+  value = aws_api_gateway_deployment.apigw.id
 }
 
 output "domain_name" {
-  value = "${aws_api_gateway_domain_name.apigw.*.domain_name}"
+  value = aws_api_gateway_domain_name.apigw.*.domain_name
 }
 
 output "api_id" {
-  value = "${aws_api_gateway_rest_api.apigw.id}"
+  value = aws_api_gateway_rest_api.apigw.id
 }
 
 output "api_root_id" {
-  value = "${aws_api_gateway_rest_api.apigw.root_resource_id}"
+  value = aws_api_gateway_rest_api.apigw.root_resource_id
 }
 
 output "gateway_http_method" {
-  value = "${aws_api_gateway_method.main.http_method}"
+  value = aws_api_gateway_method.main.http_method
 }
 
 output "gateway_resource_id" {
-  value = "${aws_api_gateway_method.main.resource_id}"
+  value = aws_api_gateway_method.main.resource_id
 }
+
