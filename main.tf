053b3ebdb5e81dd381c6f5320285bd0c4bc67fff
Eun Song <songew81@gmail.com>
2021-10-22 15:34:31 +0900

    wip

--- main.tf
+++ main.tf
@@ -66,6 +66,33 @@ resource "aws_api_gateway_deployment" "apigw" {
   stage_name  = var.stage
 }
 
+resource "aws_api_gateway_method" "options" {
+  rest_api_id   = aws_api_gateway_rest_api.apigw.id
+  resource_id   = aws_api_gateway_resource.main.id
+  http_method   = "OPTIONS"
+  authorization = var.authorization
+}
+
+resource "aws_api_gateway_integration" "options" {
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  resource_id = aws_api_gateway_method.options.resource_id
+  http_method = aws_api_gateway_method.options.http_method
+
+  # The integration HTTP method specifying how API Gateway 
+  # will interact with the back end. Not all methods are 
+  # compatible with all AWS integrations. e.g. 
+  # Lambda function can only be invoked via POST.
+  integration_http_method = var.integration_http_method
+  type                    = var.integration_type
+  uri                     = var.lambda_invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "apigw2" {
+  depends_on  = [aws_api_gateway_integration.options]
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  stage_name  = var.stage
+}
+
 ###########################################################
 ## Allowing API Gateway to Access Lambda
 ###########################################################
